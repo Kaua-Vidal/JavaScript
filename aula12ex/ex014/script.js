9a3807318b384f19ef5df2c21bfcc75b639825a2
Kaua-Vidal <kaua.vidal@dcx.ufpb.br>
2025-05-15 16:38:36 -0300

    Adicionando exercicios

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,23 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        img.src = 'manha.png'
+        document.body.style.background = '#fed698'
+        document.querySelector("header").style.color = "black"
+        document.querySelector("footer").style.color = "black"
+    } else if (hora >= 12 && hora < 18){
+        img.src = 'tarde.png'
+        document.body.style.background = '#fc6f03'
+    } else {
+        img.src = 'noite.png'
+        document.body.style.background = '#002347'
+    }
+
+}
+
+
