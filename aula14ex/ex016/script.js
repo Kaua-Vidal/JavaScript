c30a8dd792a794c7d069904fb0bc5a6b5b9d372f
Kaua-Vidal <kaua.vidal@dcx.ufpb.br>
2025-05-21 01:17:59 -0300

    ex016

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,35 @@
-alert('Ola')
+function contar() {
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = "Impossivel contar!"
+        //window.alert('[ERRO] Faltam dados!')
+        
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if (p <= 0) {
+            window.alert ('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+
+        if(i < f) {
+            for (var c = i; c <= f; c += p) {
+            res.innerHTML += `${c} ➜ `
+            }
+        } else {
+            for (var c = i; c >= f; c -= p){
+                res.innerHTML += `${c} ➜ `
+            } 
+        }
+
+        
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
