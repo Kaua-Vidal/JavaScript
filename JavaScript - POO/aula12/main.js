7256539bec631e790adef2baeee13ad21e167662
Kaua-Vidal <kaua.vidal@dcx.ufpb.br>
2025-05-29 23:02:58 -0300

    Menipulando elementos DOM - JavaScript POO

---
+++ JavaScript - POO/aula12/main.js
@@ -0,0 +1,21 @@
+// SELECIONAR ALGUM ELEMENTO
+// const addUserText = document.getElementById('add-user')
+// const addUserText = document.querySelector('#add-user')
+
+// SELECIONAR MULTIPLOS ELEMENTOS
+// const allItems = document.querySelectorAll('.item')
+// const allItems = document.getElementsByClassName('item')
+// const allItems = document.getElementsByTagName('li')
+
+const items = document.querySelector('.items')
+const button = document.querySelector('.btn')
+//items.remove()
+
+// items.firstElementChild.remove()
+// items.lastElementChild.remove()
+
+// items.children[0].textContent = 'Item Um'
+
+// items.lastElementChild.innerHTML = '<h1> Hello World!</h1>'
+
+button.style.background = 'red'
